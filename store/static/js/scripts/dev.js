2d81c1fb3bf8c6649b670f1fd4729b4871a566c1
Jeff Corcoran <jcorcoran+github@gmail.com>
2017-09-28 20:46:15 -0400

    Adjust json and variable names

--- store/static/js/scripts/dev.js
+++ store/static/js/scripts/dev.js
@@ -1,26 +1,3 @@
-
-/** @namespace logged_in
- *  @type boolean
- * */
-
-/** @namespace search_term
- *  @type string
- * */
-
-/** @namespace StoreUrl
- *  @type Object
- *  @property {function} getSort
- *  @property {function} addCategory
- *  @property {function} addFilters
- *  @property {function} addQueryParams
- *  @property {function} getSort
- *  @property {function} newUrl
- *  @property {function} setSort
- *  @property {function} setCategory
- *  @property {function} setAuthor
- *  @property {function} setFilter
- * */
-
 const categoryUrl = "/store/recent/";
 const appUrl = "/store/app/";
 
